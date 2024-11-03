7add94e7638c9d4da7fc066ed1f9ffbc5f2ecdf5
iKeKox <benjarivas19@gmail.com>
2024-11-03 18:18:40 -0300

    gestion_estudiantes main.py

--- Main.py
+++ Main.py
@@ -1,26 +1,24 @@
 #Colocar en un futuro los import
 from gestion_estudiantes import *
-
-
-def menu():
-    while True:
-        print('''
-            1°- Registrar Estudiantes
-            2° - Listar Estudiantes                 
-            3° - Modificar informacion
-            4° - Obtener Promedio de un estudiante
-            5° - Cerrar Programa
-              ''')
-        opcion = int(input("Ingrese alguna de las opciones disponibles :"))
-        if opcion == 1:
-            pass
-        elif opcion == 2:
-            pass
-        elif opcion == 3:
-            pass
-        elif opcion == 4:
-            pass
-        elif opcion == 5:
-            break
-
-menu()
+estudiantes=[]
+while True:
+    print('''
+        1°- Registrar Estudiantes
+        2° - Listar Estudiantes                 
+        3° - Modificar informacion
+        4° - Obtener Promedio de un estudiante
+        5° - Cerrar Programa
+        ''')
+    opcion = int(input("Ingrese alguna de las opciones disponibles :"))
+    if opcion == 1:
+        estudiantes.append(registro_estudiante())
+        pass        
+    elif opcion == 2:
+        listar_estudiantes(estudiantes)
+        pass        
+    elif opcion == 3:
+        pass
+    elif opcion == 4:
+        pass
+    elif opcion == 5:
+        break
--- gestion_estudiantes.py
+++ gestion_estudiantes.py
@@ -1,6 +1,12 @@
 def registro_estudiante():
     Nombre = input("Ingrese Primer Nombre del Estudiante: ")
     Apellido = input("Ingrese Primer Apellido del Estudiante: ")
-    Rut = int(input("..."))
+    Rut = int(input("Ingrese rut (sin puntos,ni guion)"))
     matricula = id(Rut)
     return Nombre, Apellido, Rut, matricula
+
+def listar_estudiantes(estudiantes):
+    for i in range(len(estudiantes)):
+        print(f"Nombre{estudiantes[i][0]} {estudiantes[i][1]} Rut {estudiantes[i][2]} Matricula {estudiantes[i][3]}")
+def modificar_estudiantes():
+    pass
