3b97f99234f5d7833d65b88f7ef4422a4106a667
Somebody <ewigkt.untergehen0@yahoo.com>
2024-11-03 18:15:54 -0300

    Una forma de guardar en tuplas

--- gestion_estudiantes.py
+++ gestion_estudiantes.py
@@ -1,6 +1,16 @@
-def registro_estudiante():
+# def registro_estudiante():
+#     Nombre = input("Ingrese Primer Nombre del Estudiante: ")
+#     Apellido = input("Ingrese Primer Apellido del Estudiante: ")
+#     Rut = int(input("..."))
+#     matricula = id(Rut)
+#     return Nombre, Apellido, Rut, matricula
+estudiantes = dict()
+cant = int(input("Cuantos estudiantes desea crear: "))
+for i in range(0,cant):
     Nombre = input("Ingrese Primer Nombre del Estudiante: ")
     Apellido = input("Ingrese Primer Apellido del Estudiante: ")
     Rut = int(input("..."))
     matricula = id(Rut)
-    return Nombre, Apellido, Rut, matricula
+    estudiantes["est" + str(i)] = (Nombre, Apellido, Rut, matricula)
+    
+print(estudiantes)
