3810a8567d5a71c61724c6d61875d5c76bd6fecf
Somebody <ewigkt.untergehen0@yahoo.com>
2024-11-03 16:23:49 -0300

    Creacion del menu
    A grandez rasgos, lo mas basico

---
+++ Main.py
@@ -0,0 +1,24 @@
+#Colocar en un futuro los import
+
+
+
+
+while True:
+    opcion = int(input('''
+        ingrese una de las siguientes opciones:
+        1° - Registrar Estudiante
+        2° - Listar Estudiantes                 
+        3° - Modificar informacion
+        4° - Obtener Promedio de un estudiante
+        5° - Cerrar Programa
+                         '''))
+    if opcion == 1:
+        pass
+    elif opcion == 2:
+        pass
+    elif opcion == 3:
+        pass
+    elif opcion == 4:
+        pass
+    elif opcion == 5:
+        break
