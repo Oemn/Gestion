6e0a0fb7a20c30c0425073edc32c1d35b3a29ef2
Somebody <ewigkt.untergehen0@yahoo.com>
2024-11-03 16:57:46 -0300

    Create gestion_estudiantes.py

---
+++ gestion_estudiantes.py
@@ -0,0 +1,6 @@
+def registro_estudiante():
+    Nombre = input("Ingrese Primer Nombre del Estudiante: ")
+    Apellido = input("Ingrese Primer Apellido del Estudiante: ")
+    Rut = int(input("..."))
+    matricula = id(Rut)
+    return Nombre, Apellido, Rut, matricula
