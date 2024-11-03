84c3c8fb113cc3394d485e592bf1b65fa25bad9f
Somebody <ewigkt.untergehen0@yahoo.com>
2024-11-03 16:42:15 -0300

    Pequeña modifficacion

--- Main.py
+++ Main.py
@@ -2,23 +2,25 @@
 
 
 
+def menu():
+    while True:
+        print('''
+            1°- Registrar Estudiantes
+            2° - Listar Estudiantes                 
+            3° - Modificar informacion
+            4° - Obtener Promedio de un estudiante
+            5° - Cerrar Programa
+              ''')
+        opcion = int(input("Ingrese alguna de las opciones disponibles :"))
+        if opcion == 1:
+            pass
+        elif opcion == 2:
+            pass
+        elif opcion == 3:
+            pass
+        elif opcion == 4:
+            pass
+        elif opcion == 5:
+            break
 
-while True:
-    opcion = int(input('''
-        ingrese una de las siguientes opciones:
-        1° - Registrar Estudiante
-        2° - Listar Estudiantes                 
-        3° - Modificar informacion
-        4° - Obtener Promedio de un estudiante
-        5° - Cerrar Programa
-                         '''))
-    if opcion == 1:
-        pass
-    elif opcion == 2:
-        pass
-    elif opcion == 3:
-        pass
-    elif opcion == 4:
-        pass
-    elif opcion == 5:
-        break
+menu()
