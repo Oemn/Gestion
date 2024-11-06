1404b0ea0767b79f2661c64512c2db4a1336db8b
iKeKox <benjarivas19@gmail.com>
2024-11-06 19:04:27 -0300

    Ola
    Ola

--- gestion_cursos.py
+++ gestion_cursos.py
@@ -1,7 +1,10 @@
 def listar_cursos(cursos):
+    for i in range(len(cursos)): print(f"Cursos:{i[cursos]}")
     pass
 def modificar_cursos(cursos):
     pass
 def eliminar_cursos(cursos):
     #antes de eliminar lo añadimos al respaldo db_sys
+    pass
+def asignar_cursos():
     pass
--- gestion_estudiantes.py
+++ gestion_estudiantes.py
@@ -1,19 +1,5 @@
 import pandas as pd
 from eliminaciones import *
-# from gestion_cursos import *
-# def registrar_estudiante(estudiantes):
-#     cant = int(input("Cuantos estudiantes desea crear: "))
-    
-#     for i in range(len(estudiantes)+1,cant+(len(estudiantes))+1):
-        
-#         Nombre = input("Ingrese Primer Nombre del Estudiante: ")
-#         Apellido = input("Ingrese Primer Apellido del Estudiante: ")
-#         Rut = int(input("Ingrese rut (sin puntos,ni guion: )"))
-#         for i in range(len(estudiantes)+100): matricula = f"N°{i}""
-#         estudiantes["est" + str(i)] = (Nombre, Apellido, Rut, matricula)
-#         asignaturas()
-
-#     print(estudiantes)
 
 def listar_estudiantes(estudiantes):
     df = pd.DataFrame(data = estudiantes)
@@ -24,12 +10,9 @@ def registrar_estudiante(estudiantes):
         Rut = int(input("Ingrese rut (sin puntos,ni guion: )"))
         for e in range(len(estudiantes)+1001): matricula = f"N°{e}"
         return {
-            "Estudiante" : (Nombre_c, Rut, matricula)
+            "Estudiante" : (Nombre_c, Rut, matricula), "Curso":"","Notas":"","Sede":""
         }
-def modificar_nota_estudiantes(estudiantes):
-    matricula_ver=int(input("Ingrese matricula de estudiante a modificar"))
     #se tendria que modificar la nota del estudiante
-    pass
 def eliminar_estudiantes(estudiantes):
     pass
     # while True:
