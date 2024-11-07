c4eec78eb3274715b2e049d5f8956ec562df39c5
iKeKox <benjarivas19@gmail.com>
2024-11-07 18:23:00 -0300

    borrar innecesario

--- Main.py
+++ Main.py
@@ -1,33 +1,3 @@
-# Colocar en un futuro los import
-# import json
-# from gestion_estudiantes import *
-# estudiantes = dict()
-# while True:
-#     print('''
-#         1°- Registrar Estudiantes
-#         2° - Listar Estudiantes                 
-#         3° - Modificar informacion
-#         4° - Eliminar
-#         5° - Obtener Promedio de un estudiante
-#         6° - Cerrar Programa
-#         ''')
-#     opcion = int(input("Ingrese alguna de las opciones disponibles: "))
-#     if opcion == 1:
-#         añadir un menu de en que registrar si estudiantes ,curso
-#         continue        
-#     elif opcion == 2:
-#         añadir menu si listar estudiantes solamente, + notas , +establecimiento
-#         pass        
-#     elif opcion == 3:
-#         añadir si modificar notas, estudiantes, (nose que mas)
-#         pass
-#     elif opcion == 4:
-#         Añadir si eliminar un curso , estudiante , notas , asignatura
-#         pass
-#     elif opcion == 5:
-#         break
-#     elif opcion == 6:
-#         break
 import pandas as pd
 from gestion_estudiantes import *
 from datos_estudiantes import *
