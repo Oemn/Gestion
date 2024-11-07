9e34ba545033d61ca43f927aabd38c1e40c288f5
Somebody <ewigkt.untergehen0@yahoo.com>
2024-11-07 19:09:44 -0300

    .

--- gestion_estudiantes.py
+++ gestion_estudiantes.py
@@ -28,4 +28,4 @@ def eliminar_estudiantes(estudiantes):
                         print("Estudiante no encontrado.")
 def listar_estudiantes(estudiantes):
     df = pd.DataFrame(data = estudiantes)
-    return print(df)
+    return print(df.rename(columns = {"Estudiante" : "Informacion Estudiante", "Curso" : "Asignaturas a cursar"}))
