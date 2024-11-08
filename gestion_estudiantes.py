7764384577ca9db1385fffeae5541ad97f0b2bf0
iKeKox <benjarivas19@gmail.com>
2024-11-07 21:03:43 -0300

    validacion estudiantes, nuevamente

--- gestion_estudiantes.py
+++ gestion_estudiantes.py
@@ -3,15 +3,29 @@ from eliminaciones import *
 
 
 def registrar_estudiante(estudiantes):
-
-        Nombre_c = input("Ingrese Nombre: ")
-        Rut = int(input("Ingrese rut (sin puntos,ni guion: )"))
-        for e in range(len(estudiantes)+1001): matricula = f"N°{e}"
-        return {
-            "Estudiante" : (Nombre_c, Rut, matricula), "Curso": [],"Notas":"","Sede":""
-        }
-    #verificar si existe el rut 
+    loop=True
+    existe=False
+    Nombre_c = input("Ingrese Nombre: ")
+    while loop:
+        try:
+            Rut = int(input("Ingrese rut (sin puntos,ni guion: )"))
+            for i in range(len(estudiantes)):
+                if Rut == estudiantes[i]['Estudiante'][1]:
+                    print("Ya existe el rut.")
+                    existe=True
+                    break#si existe rompe el bucle FOR con el break, para que asi en la siguiente iteracion no cambie el valor de existe a =False y pase
+                else:
+                    existe=False
+            if existe == False:
+                break
+        except:
+            print("Valor ingresado incorrecto.")
+    for e in range(len(estudiantes)+1001): matricula = f"N°{e}"
+    return {
+        "Estudiante" : (Nombre_c, Rut, matricula), "Curso": [],"Notas":"","Sede":""
+    }
 def eliminar_estudiantes(estudiantes):
+    #revisar si existen estudiantes antes de hacerlo
     while True:
         borrar=int(input("Ingrese matricula estudiante: "))
         for i in range(len(estudiantes)):
